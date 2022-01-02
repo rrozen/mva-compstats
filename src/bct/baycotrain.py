629f7931f291c126948f7ec5d8a0916d8550e781
Skidijo <rpl159@laposte.net>
2022-01-02 12:15:20 +0100

    hyper optim

--- src/bct/baycotrain.py
+++ src/bct/baycotrain.py
@@ -1,42 +1,49 @@
 import numpy as np
-from sklearn.gaussian_process.kernels import Kernel, RBF  # type: ignore
+from typing import Tuple, List
+from sklearn.gaussian_process.kernels import Kernel, RBF, Hyperparameter  # type: ignore
 
 
-def compute_Kc(x, sigs=None, rho=1 / np.sqrt(2)):
+def compute_Kc(
+    x: np.ndarray, sig2s=None, rho: float = 1 / np.sqrt(2)
+) -> Tuple[np.ndarray, List[np.ndarray]]:
     n, d = x.shape
     ks = [RBF(rho)(x[:, i, None]) for i in range(d)]
-    if sigs is None:
-        sigs = [1e-3] * d
-    return np.linalg.inv(
-        sum([np.linalg.inv(k + (sig ** 2) * np.identity(n)) for (k, sig) in zip(ks, sigs)])
-    )
+    if sig2s is None:
+        sig2s = [1] * d
+    invs = [np.linalg.inv(k + sig2 * np.identity(n)) for (k, sig2) in zip(ks, sig2s)]
+    return np.linalg.inv(sum(invs)), invs
 
 
-class consensus(Kernel):
-    def __init__(self, sigs, rho=1):
+class Consensus(Kernel):
+    def __init__(self, sig2s, sig2s_bounds, rho=1):
         self.rho = rho
-        self.sigs = sigs
+        self.sig2s = sig2s
+        self.sig2s_bounds = sig2s_bounds
+
+    @property
+    def hyperparameter_length_scale(self):
+        return Hyperparameter("sig2s", "numeric", self.sig2s_bounds, len(self.sig2s))
 
     def __call__(self, X, Y=None, eval_gradient=False):
         if Y is None:
-            return compute_Kc(X, self.sigs, self.rho)
+            Kc, invs = compute_Kc(X, self.sig2s, self.rho)
         else:
+            if eval_gradient:
+                raise ValueError("Gradient can only be evaluated when Y is None.")
             ntrain, _ = X.shape
-            Kc = compute_Kc(np.concatenate((X, Y)), self.sigs, self.rho)
-            return Kc[:ntrain, ntrain:]
+            Kc, invs = compute_Kc(np.concatenate((X, Y)), self.sig2s, self.rho)
+            Kc = Kc[:ntrain, ntrain:]
+
+        if eval_gradient:
+            Kc_gradient = np.stack(
+                [Kc @ inv @ inv @ Kc / sig2 for inv, sig2 in zip(invs, self.sig2s)], axis=-1
+            )
+            return Kc, Kc_gradient
+        else:
+            return Kc
 
     def is_stationary(self):
         return False
 
     def diag(self, X):
         pass
-
-
-def regression(x_train, y_train, x_test):
-    ntrain = len(x_train)
-    x = np.concatenate((x_train, x_test))
-    Kc = compute_Kc(x, [1e-3, 1e3])
-    K_train = Kc[:ntrain, :ntrain]
-    K_test = Kc[:ntrain, ntrain:]
-    y_test = K_test.T @ K_train @ y_train
-    return y_test
